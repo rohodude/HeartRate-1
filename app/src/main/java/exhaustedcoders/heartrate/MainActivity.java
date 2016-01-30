33b47b550cb55fc81d6c56940cb43c080458d7d7
rohodude <rohodude@gmail.com>
2016-01-30 11:55:22 -0700

    Working on settings

--- app/src/main/java/exhaustedcoders/heartrate/MainActivity.java
+++ app/src/main/java/exhaustedcoders/heartrate/MainActivity.java
@@ -1,14 +1,19 @@
 package exhaustedcoders.heartrate;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
+
+import java.util.Set;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,12 +21,25 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.listView) ListView listView;
     @Bind(R.id.imageButton) ImageButton imageButton;
+    @Bind(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(MainActivity.this);
+        toolbar.setTitle("Toolbar");
+        toolbar.inflateMenu(R.menu.settings);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.settings) {
+                    Intent intent = new Intent(MainActivity.this, Settings.class);
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -39,8 +57,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.settings) {
+            Intent intent = new Intent(this, Settings.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/exhaustedcoders/heartrate/Settings.java
+++ app/src/main/java/exhaustedcoders/heartrate/Settings.java
@@ -1,10 +1,18 @@
 package exhaustedcoders.heartrate;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by cidseuser on 1/30/2016.
  */
-public class Settings extends Activity{
-
+public class Settings extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+        ButterKnife.bind(this);
+    }
 }
