e95d558f4b42a0e252727f0271de4703b80327cd
rohodude <rohodude@gmail.com>
2016-01-30 09:51:22 -0700

    Just a test

--- app/src/main/java/exhaustedcoders/heartrate/MainActivity.java
+++ app/src/main/java/exhaustedcoders/heartrate/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+    //onCreateOptionsMenu
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
