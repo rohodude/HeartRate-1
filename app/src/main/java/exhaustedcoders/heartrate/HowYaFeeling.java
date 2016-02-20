90b3a7c6648623b739c320df653ff6c7897b2d9a
rohodude <rohodude@gmail.com>
2016-02-20 11:02:00 -0700

    I'm too lazy to explain #2

---
+++ app/src/main/java/exhaustedcoders/heartrate/HowYaFeeling.java
@@ -0,0 +1,27 @@
+package exhaustedcoders.heartrate;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by cidseuser on 2/20/2016.
+ */
+public class HowYaFeeling extends Activity {
+    private int bpm = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.howyafeeling);
+        ButterKnife.bind(HowYaFeeling.this);
+        TextView textView;
+
+        Intent intent = getIntent();
+        bpm = intent.getIntExtra("BPM", 0);
+    }
+
+
+}
